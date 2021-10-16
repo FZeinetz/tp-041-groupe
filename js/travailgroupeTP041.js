c099b9abb64e66b22d22ce3309c58aefe2dc9aa2
TanguyPatron <92632964+TanguyPatron@users.noreply.github.com>
2021-10-16 15:16:53 +0200

    Update travailgroupeTP041.js

--- js/travailgroupeTP041.js
+++ js/travailgroupeTP041.js
@@ -1,6 +1,15 @@
 "use strict" ;
 
 /**
+*Mastermind 
+* Définir les couleurs 
+* L'ordinateur crée un array avec 4 couleurs aléatoires
+* On demande à l'utilisateur d'entrer 4 couleurs dans un autre array
+* L'ordinateur lui dit quel(s) élément(s) correspond(ent)
+* On boucle 10 fois ou jusqu'à ce que le joueur ait gagné
+*/
+
+/**
  * Fonction qui...
  *
  * @example
