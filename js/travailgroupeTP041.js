45cff7f9427e4681189008c9bb807ded333ef4ac
MartinHayot <92633418+MartinHayot@users.noreply.github.com>
2021-10-16 15:18:08 +0200

    Create travailgroupeTP041.js

--- js/travailgroupeTP041.js
+++ js/travailgroupeTP041.js
@@ -1,15 +1,6 @@
 "use strict" ;
 
 /**
-*Mastermind 
-* Définir les couleurs 
-* L'ordinateur crée un array avec 4 couleurs aléatoires
-* On demande à l'utilisateur d'entrer 4 couleurs dans un autre array
-* L'ordinateur lui dit quel(s) élément(s) correspond(ent)
-* On boucle 10 fois ou jusqu'à ce que le joueur ait gagné
-*/
-
-/**
  * Fonction qui...
  *
  * @example
@@ -19,6 +10,7 @@
  * @return {}
  *
  */
-function travailGroupe() {
+function travailGroupe()
+{
 	
 }
